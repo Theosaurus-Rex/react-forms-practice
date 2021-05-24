356fd2b28f5c2175032a69e7e5bbc59f6336a0d4
Theosaurus-Rex <theosaurus13@gmail.com>
2021-05-25 08:34:52 +1000

    implement form submission logic

--- src/App.js
+++ src/App.js
@@ -13,12 +13,53 @@ const users = [
 ]
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      errorMessage: "",
+      successMessage: ""
+
+    }
+
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      errorMessage: ""
+    })
+    const formElements = event.target.children
+    const emailInput = formElements[1]
+    const passwordInput = formElements[3]
+    const emailValue = emailInput.value
+    const passwordValue = passwordInput.value
+    const foundUser = users.find((user) => user.email === emailValue)
+    if (foundUser) {
+      if (passwordValue === foundUser.password) {
+        this.setState({
+          successMessage: "User successfully authenticated!"
+        })
+      } else {
+        this.setState({
+          errorMessage: "Wrong credentials have been entered."
+        })
+      }
+    } else {
+      this.setState({
+        errorMessage: "Wrong credentials have been entered."
+      })
+    }
+  }
+
   render() {
     console.log(users)
     return (
       <div className="container">
+        {this.state.errorMessage && <p>{this.state.errorMessage}</p>}
+        {this.state.successMessage && <p>{this.state.successMessage}</p>} 
         <h2>Login</h2>
-        <form>
+        <form onSubmit={this.onFormSubmit}>
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="email" />
           <label htmlFor="password">Password</label>
